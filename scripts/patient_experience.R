f70e6142cc78b57bb1001cf13a755ca2b2bbf20e
Iruoma <s2487042@ed.ac.uk>
2025-11-12 10:48:56 +0000

    Change in experince

--- scripts/patient_experience.R
+++ scripts/patient_experience.R
@@ -6,10 +6,10 @@ data("cms_patient_experience", package = "tidyr")
 orgs <- c('TRINITY WEST', 'TRINITY HEALTH', 'REX HOSPITAL INC', 'HOLZER CLINIC LLC')
 
 exp_summary <- cms_patient_experience |>
-  filter(facility_name %in% org)
+  filter(org_nm %in% org_nm)
 
 exp_summary
 
-ggplot(exp_summary, aes(prf_rate, facility, fill = measure_cd)) +
+ggplot(exp_summary, aes(prf_rate, org_nm, fill = measure_cd)) +
   geom_col(position = "dodge")
 
